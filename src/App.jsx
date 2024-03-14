ddd81cdd6504972f97e123f87dfd2812be06dcb4
Gopi2923 <dgopidhikshan@gmail.com>
2024-03-14 12:19:30 +0530

    Code format.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import Todo from "./Components/Todo"
-
+import Todo from "./Components/Todo";
 
 const App = () => {
-
-  
-
   return (
     <div>
-       <Todo />
+      <Todo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
