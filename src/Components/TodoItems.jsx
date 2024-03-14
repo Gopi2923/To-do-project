d96dd00f0e83473326019c897d68fd65ab181fc4
Gopi2923 <dgopidhikshan@gmail.com>
2024-03-14 08:54:46 +0530

    changes

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,44 +1,54 @@
-import './CSS/TodoItems.css'
-import cross from './Assets/cross.png'
-import not_tick from './Assets/not_tick.png'
-import tick from './Assets/tick.png'
+import "./CSS/TodoItems.css";
+import cross from "./Assets/cross.png";
+import not_tick from "./Assets/not_tick.png";
+import tick from "./Assets/tick.png";
 
-const TodoItems = ({no, display, text, setTodos}) => {
-
+const TodoItems = ({ no, display, text, setTodos }) => {
   const deleteTodo = (no) => {
-    let data = JSON.parse(localStorage.getItem('todos'));
-data = data.filter((todo) => todo.no !== no);
-setTodos(data)
-  }
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todo) => todo.no !== no);
+    setTodos(data);
+  };
   const toggle = (no) => {
-    let data = JSON.parse(localStorage.getItem('todos'));
-    for(let i = 0; i < data.length; i++)
-    {
-      if (data[i].no===no) {
-        if (data[i].display==='') {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        if (data[i].display === "") {
           data[i].display = "line-through";
-        }
-        else{
+        } else {
           data[i].display = "";
         }
         break;
       }
     }
     setTodos(data);
-  }
+  };
   return (
-   <div className="todoitems">
-    <div className={`todoitems-container ${display}`} onClick={() => {toggle(no)}}>
-        {display=== "" ? <img src={not_tick} alt=''/> : <img src={tick} alt="" />}
-        
-       
-        <div className="todoitems-text">
-           {text}
-        </div>
-    </div>
-    <img className='totoitems-cross-icon' onClick={() => {deleteTodo(no)}} src={cross} alt="" />
-   </div>
-  )
-}
+    <div className="todoitems">
+      <div
+        className={`todoitems-container ${display}`}
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img src={not_tick} alt="" />
+        ) : (
+          <img src={tick} alt="" />
+        )}
 
-export default TodoItems
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img
+        className="totoitems-cross-icon"
+        onClick={() => {
+          deleteTodo(no);
+        }}
+        src={cross}
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default TodoItems;
